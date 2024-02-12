dc20aec49e7a4c9f143c6a2d743d6e69f84bcfc7
Devlynn-E <141595269+Devlynn-E@users.noreply.github.com>
2024-02-12 14:13:56 +1300

    added integer checker >= 13

--- integer checker.py
+++ integer checker.py
@@ -1,10 +1,18 @@
 
 while True:
 
+    error = "please enter an integer 13 or above"
     try:
+        # checks for an integer
         my_num = int(input("Enter an integer "))
-        print("Your number is ", my_num)
+
+        # checks that the number >= 13
+        if my_num < 13:
+            print(error)
+            
+        elif my_num >= 13:
+            print("Your number is ", my_num)
 
     except ValueError:
-        print("please enter an integer ")
+        print(error)
         
