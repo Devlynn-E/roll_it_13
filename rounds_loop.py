b2b023b25531d81cb01ca1e0276ab9a893ffea76
Devlynn-E <141595269+Devlynn-E@users.noreply.github.com>
2024-02-22 09:58:01 +1300

    updated rounds loop

--- rounds_loop.py
+++ rounds_loop.py
@@ -1,3 +1,33 @@
+import random
+
+
+def roll():
+    # grab a random number between 1 - 6
+    result = random.randint(1, 6)
+    return result
+
+
+def two_rolls(who):
+    # rolls 2 dice amd returns total + if we had a double roll
+
+    double_score = "no"
+
+    # rolls 2 dice
+    roll_1 = roll()
+    roll_2 = roll()
+
+    # check for double score
+    if roll_1 == roll_2:
+        double_score = "yes"
+
+    # find total points
+    total_points = roll_1 + roll_2
+
+    # show result
+    print(f"{who}: {roll_1} & {roll_2} total: {total_points}")
+
+    return total_points, double_score
+
 
 def int_check(question):
     while True:
@@ -23,10 +53,19 @@ def int_check(question):
 user_score = 0
 com_score = 0
 
+num_rounds = 0
+
 target_score = int_check("enter a target score: ")
 print(target_score)
 
 while user_score < target_score and com_score < target_score:
-    print("Round heading goes here...")
+
+    # add one to the number of rounds (for our heading)
+    num_rounds += 1
+    print(f"*** Round {num_rounds} ***")
+
     add_points = int(input("How many points have been won?"))
     user_score += add_points
+
+print()
+print(f"Your final score is {user_score}")
